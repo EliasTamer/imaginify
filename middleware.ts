9b89bc4403589104f383746b1b9f8a8924a3fcbf
Elias Tamer <eliastamer3@gmail.com>
2024-03-03 10:41:30 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,18 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks(.*)", "/api/webhooks/stripe"],
+  publicRoutes: [
+    "/",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
+  ignoredRoutes: [
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
 });
 
 export const config = {
